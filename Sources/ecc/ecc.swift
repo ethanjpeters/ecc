505bbb1de4375d35b82721d0d8eef82af3525ff6
Ethan Peters <ethanjamespeters@gmail.com>
2025-08-13 15:25:45 -0400

    rudimentary parser

--- Sources/ecc/ecc.swift
+++ Sources/ecc/ecc.swift
@@ -42,9 +42,10 @@ extension String {
 enum ExitCode: Int32 {
     case ioError = 3
     case lexerError = 4
+    case parserError = 5
 }
 
-enum Token {
+enum Token : Equatable {
     case keywordInt
     case keywordVoid
     case keywordReturn
@@ -158,6 +159,104 @@ func lexFile(sourceFile: String) -> [Token] {
     return out
 }
 
+// Recursive descent parser
+
+// Parser namespace/scope
+
+struct Parser {
+    struct AST {
+        enum Expression {
+            case Constant(Int)
+        }
+
+        enum Statement {
+            case Return(Expression)
+        }
+
+        enum Program {
+            case Function(String /* name */, Statement /* body */)
+        }
+    }
+}
+
+func expect(_ tok: Token, _ tokenStream: inout [Token]) -> Token {
+    if tokenStream.isEmpty {
+        print("Expected \(tok) but encountered end of token stream")
+        exit(ExitCode.parserError.rawValue)
+    }
+
+    let nextToken = tokenStream.removeFirst()
+    if nextToken != tok {
+        print("Expected \(tok) but encountered \(nextToken)")
+        exit(ExitCode.parserError.rawValue)
+    }
+
+    return nextToken
+}
+
+func parseExpression(tokenStream: inout [Token]) -> Parser.AST.Expression {
+    if tokenStream.isEmpty {
+        print("Expected integer but encountered end of token stream")
+        exit(ExitCode.parserError.rawValue)
+    }
+
+    let integer = tokenStream.removeFirst()
+    switch integer {
+        case .constant(let val):
+            guard let intVal = Int(val) else {
+                print("Integer constant \(val) was not a valid integer")
+                exit(ExitCode.parserError.rawValue)
+            }
+            return .Constant(intVal)
+        default:
+            print("Expected integer but encountered \(integer)")
+            exit(ExitCode.parserError.rawValue)
+    }
+}
+
+func parseStatement(tokenStream: inout [Token]) -> Parser.AST.Statement {
+    let _ = expect(.keywordReturn, &tokenStream)
+    let exp = parseExpression(tokenStream: &tokenStream)
+    let _ = expect(.semicolon, &tokenStream)
+
+    return .Return(exp)
+}
+
+func parseProgram(tokenStream: inout [Token]) -> Parser.AST.Program {
+    if tokenStream.isEmpty {
+        print("Empty token stream encountered when expecting a program")
+        exit(ExitCode.parserError.rawValue)
+    }
+
+    let _ = expect(.keywordInt, &tokenStream)
+
+    if tokenStream.isEmpty {
+        print("Empty token stream encountered when expecting a function identifier")
+        exit(ExitCode.parserError.rawValue)
+    }
+
+    let idToken = tokenStream.removeFirst()
+    let functionName : String
+    switch idToken {
+        case .identifier(let name):
+            functionName = name
+        default:
+            print("Expected function identifier but encountered \(idToken)")
+            exit(ExitCode.parserError.rawValue)
+    }
+
+    let _ = expect(.openParen, &tokenStream)
+    let _ = expect(.keywordVoid, &tokenStream)    // currently the only acceptable parameter type
+    let _ = expect(.closeParen, &tokenStream)
+    let _ = expect(.openBrace, &tokenStream)
+
+    let statement = parseStatement(tokenStream: &tokenStream)
+
+    let _ = expect(.closeBrace, &tokenStream)
+
+    return .Function(functionName, statement)
+}
+
 @main
 struct ECC : ParsableCommand {
 
@@ -187,19 +286,19 @@ struct ECC : ParsableCommand {
 
         // lex
 
-        let tokenStream = lexFile(sourceFile: preprocFile)
-
-        // DEBUG
-        tokenStream.forEach { tkn in
-            print(tkn)
-        }
-        // END DEBUG
+        var tokenStream = lexFile(sourceFile: preprocFile)
 
         if lex {
             return
         }
 
-        // TODO: parse
+        // parse
+
+        let ast = parseProgram(tokenStream: &tokenStream)
+
+        if parse {
+            return
+        }
 
         // TODO: code gen
 
