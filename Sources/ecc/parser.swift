916edb1404d74f290e98e374eedc1aa0c38430d8
Ethan Peters <ethanjamespeters@gmail.com>
2025-09-13 11:01:13 -0400

    parse comma correctly

--- Sources/ecc/parser.swift
+++ Sources/ecc/parser.swift
@@ -290,11 +290,17 @@ class Parser {
     func parseFunctionCallParameters(tokenStream: inout [Lexer.Token]) -> Parser.AST.Expression {
         let _ = expect(.openParen, &tokenStream)
         var expressionList : [Parser.AST.Expression] = []
+        var firstPass = true
         while peek(tokenStream) != .closeParen {
-            expressionList.append(parseExpression(tokenStream: &tokenStream, minimumPrecedence: 0))
             if peek(tokenStream) == .comma {
+                if firstPass {
+                    print("Found a comma at the beginning of function call parameters")
+                    exit(ExitCode.parserError.rawValue)
+                }
                 let _ = expect(.comma, &tokenStream)
             }
+            expressionList.append(parseExpression(tokenStream: &tokenStream, minimumPrecedence: 0))
+            firstPass = false
         }
         let _ = expect(.closeParen, &tokenStream)
 
